b9e2611020f4341b1fe3ace1ed8234aa4232a694
Minku Lee <premist@me.com>
2017-05-25 15:34:12 +0900

    Add an ability to customize GCS host

--- lib/gcs_signer.rb
+++ lib/gcs_signer.rb
@@ -15,16 +15,20 @@ class GcsSigner
   # gcs-signer requires credential that can access to GCS.
   # [path] the path of the service_account json file.
   # [json_string] ...or the content of the service_account json file.
+  # [gcs_host] Custom GCS host when signing a url.
   #
   # or if you also use \+google-cloud+ gem. you can authenticate
   # using environment variable that uses.
-  def initialize(path: nil, json_string: nil)
+  def initialize(path: nil, json_string: nil, gcs_host: nil)
     json_string ||= File.read(path) unless path.nil?
     json_string = look_for_environment_variables if json_string.nil?
+
 
     fail AuthError, "No credentials given." if json_string.nil?
     @credentials = JSON.parse(json_string)
     @key = OpenSSL::PKey::RSA.new(@credentials["private_key"])
+
+    @gcs_host = gcs_host || "https://storage.googleapis.com"
   end
 
   # @return [String] Signed url
@@ -58,7 +62,7 @@ class GcsSigner
   def sign_url(bucket, object_name, options = {})
     options = apply_default_options(options)
 
-    url = URI.join "https://storage.googleapis.com",
+    url = URI.join @gcs_host,
                    URI.escape("/#{bucket}/"), URI.escape(object_name)
     signature = sign string_that_will_be_signed(url, options)
     url.query = query_for_signed_url(signature, options)
